067c65b7b0b9b46cc573570d3d33247cf2f0f5b9
motosasha <moto.sasha@gmail.com>
2021-05-15 14:56:38 +0300

    + calculator page

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,8 @@
 //= ../../node_modules/jquery/dist/jquery.js
-//= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
-
+//= ../../node_modules/cleave.js/dist/cleave.js
+//= ../../node_modules/cleave.js/dist/addons/cleave-phone.ru.js
+//= ../../node_modules/air-datepicker/dist/js/datepicker.js
+//= ../../node_modules/stupid-table-plugin/stupidtable.js
 //= partials/jquery-filestyle.js
 
 //= partials/user.js
--- dev/js/partials/user.js
+++ dev/js/partials/user.js
@@ -1,13 +1,67 @@
 'use strict';
 
 $(document).ready(function() {
-	//tabs
+	// tabs
 	$('ul.tabs__caption').on('click', 'li:not(.tabs__item_active)', function() {
 		$(this).addClass('tabs__item_active').siblings().removeClass('tabs__item_active')
 			.closest('.tabs').find('.tabs__tab').removeClass('tabs__tab_active').eq($(this).index()).addClass('tabs__tab_active');
 	})
+	// input mask
+	$('.input_phone').toArray().forEach(function(field){
+		new Cleave(field, {
+			prefix: '+7 ',
+			phone: true,
+			phoneRegionCode: 'RU'
+		});
+	});
+	$('.input_sum').toArray().forEach(function(field){
+		new Cleave(field, {
+			delimiter: ' ',
+			numeral: true,
+			numeralThousandsGroupStyle: 'thousand'
+		});
+	});
 
-	$('input[type="tel"]').mask("+7 (999) 999-99-99");
+	// date picker
+	$('.input_date').datepicker();
+	$('.input_date_period').datepicker({
+		minDate: new Date(),
+		range: true,
+		multipleDatesSeparator: " – "
+	});
+
+	// calculator
+	let calcExec = $('#calcExec'),
+		calcPart = $('#calcPart'),
+		calcGO = $('#calcGO'),
+		calcVA = $('#calcVA'),
+		calcRadio = $('.calculator__radios input.checkbox__input');
+	$('[name="calcTab"]').change(function(){
+		if ($(this).val() === '0') {
+			calcExec.prop('checked', true);
+			calcPart.prop('disabled', false);
+			calcGO.prop('disabled', false);
+			calcVA.prop('disabled', true);
+			calcRadio.prop('checked', false);
+		}
+		else if ($(this).val() === '1') {
+			calcExec.prop('checked', true);
+			calcPart.prop('disabled', false);
+			calcGO.prop('disabled', false);
+			calcVA.removeAttr("disabled");
+			calcRadio.prop('checked', false);
+		}
+		else if ($(this).val() === '2') {
+			calcExec.prop('checked', true);
+			calcPart.prop('disabled', true);
+			calcGO.prop('disabled', true);
+			calcVA.prop('disabled', true);
+			calcRadio.prop('checked', false);
+		}
+	});
+
+	// table sort
+	$('.calculator-table').stupidtable();
 
 	/*
 	$('.header a.nav__link').on('click', function(event) {
